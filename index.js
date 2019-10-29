a1305231cc11e95352b43ad100288b23d48e141f
Kamil Rzeszutek <48318419+kamiljx@users.noreply.github.com>
2019-10-30 00:29:57 +0100

    ?

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+
+
 const clap = document.querySelector('#aClap');
         const chanel1 = [];
         document.body.addEventListener('keypress', playClap);
         function playClap(e){
-            if(e.code === "KeyW" || onclick(top2)){
+            if(e.code === "KeyW" || document.getElementById("aClap").click()){
                 clap.currentTime = 0;
                 clap.play();
                 const time = Date.now();
@@ -20,7 +22,7 @@ const chanel2 = [];
 const boom = document.querySelector('#aBoom');
 document.body.addEventListener('keypress', playBoom);
     function playBoom(e){
-        if(e.code === "KeyQ" || onclick(top1)){
+        if(e.code === "KeyQ" || document.getElementById('aBoom').clicked == true){
             boom.currentTime = 0;
             boom.play();
             const time = Date.now();
@@ -28,16 +30,15 @@ document.body.addEventListener('keypress', playBoom);
                 key: e.code, 
                 time: time 
             },
-        document.getElementsByClassName("top1")[0].style.backgroundColor = "red"); 
-        }
-        console.log(chanel1);
+            document.getElementsByClassName("top1")[0].style.backgroundColor = "red");         }
+        console.log(chanel2);
     
 }
 const chanel3 = [];
  const hihat = document.querySelector('#aHihat');
  document.body.addEventListener('keypress', playHihat);
       function playHihat(e){
-         if(e.code === "KeyE" || onclick(top3)){
+         if(e.code === "KeyE" || event.click === 'hihat'){
              hihat.currentTime = 0;
              hihat.play();
              const time = Date.now();
@@ -49,3 +50,26 @@ const chanel3 = [];
             }
          console.log(chanel3);
      }
+
+//RECORD//     
+
+if(document.getElementById('recordButton').clicked == true)
+{
+    document.addEventListener('DOMContentLoaded', () => {
+        'use strict';
+    
+        document.addEventListener('keydown', event => {
+            const charList = 'qweasdzxc';
+            const key = event.key.toLowerCase();
+    
+            // we are only interested in alphanumeric keys
+            if (charList.indexOf(key) === -1) return;
+    
+            console.log(key);
+        });
+    });
+    
+}
+
+
+
