a65c969949eb9203fde8a85232c1a1be688c31b4
Kamil Rzeszutek <48318419+kamiljx@users.noreply.github.com>
2019-11-06 13:34:41 +0100

    ref

--- index.js
+++ index.js
@@ -1,75 +1,171 @@
 
 
+const chanel1 = [];
 const clap = document.querySelector('#aClap');
-        const chanel1 = [];
-        document.body.addEventListener('keypress', playClap);
-        function playClap(e){
-            if(e.code === "KeyW" || document.getElementById("aClap").click()){
-                clap.currentTime = 0;
-                clap.play();
-                const time = Date.now();
-                chanel1.push({
-                    key: e.code, 
-                    time: time 
-                },
-                document.getElementsByClassName("top2")[0].style.backgroundColor = "red"); 
-            }
+const boom = document.querySelector('#aBoom');
+const ride = document.querySelector('#aRide');
+const openhat = document.querySelector('#aOpenhat');
+const kick = document.querySelector('#aKick');
+const snare = document.querySelector('#aSnare');
+const hihat = document.querySelector('#aHihat');
+const tom = document.querySelector('#aTom');
+const tink = document.querySelector('#aTink');
+document.body.addEventListener('keypress', playTink);
+document.body.addEventListener('keypress', playTom);
+document.body.addEventListener('keypress', playHihat);
+document.body.addEventListener('keypress', playClap);
+document.body.addEventListener('keypress', playSnare);
+document.body.addEventListener('keypress', playBoom);
+document.body.addEventListener('keypress', playRide);
+document.body.addEventListener('keypress', playOpenhat);
+document.body.addEventListener('keypress', playKick);
+
+    function playClap(e){
+        if(e.code === "KeyW" || e === "clap"){
+            clap.currentTime = 0;
+             clap.play();
+             const time = Date.now();
+             chanel1.push({
+                 key: e.code, 
+                 time: time 
+             },
+             document.getElementsByClassName("top2")[0].style.backgroundColor = "red"); 
+         }
+         else{
+                document.getElementsByClassName("top2")[0].style.backgroundColor = "white" 
+        }
              console.log(chanel1);
-            
         }
 
-const chanel2 = [];
-const boom = document.querySelector('#aBoom');
-document.body.addEventListener('keypress', playBoom);
     function playBoom(e){
-        if(e.code === "KeyQ" || document.getElementById('aBoom').clicked == true){
+        if(e.code === "KeyQ" || e === 'boom'){
             boom.currentTime = 0;
             boom.play();
             const time = Date.now();
-            chanel2.push({
+            chanel1.push({
                 key: e.code, 
                 time: time 
             },
-            document.getElementsByClassName("top1")[0].style.backgroundColor = "red");         }
-        console.log(chanel2);
-    
-}
-const chanel3 = [];
- const hihat = document.querySelector('#aHihat');
- document.body.addEventListener('keypress', playHihat);
+            
+            document.getElementsByClassName("top1")[0].style.backgroundColor = "blue" 
+            );    
+        }
+        else{
+                document.getElementsByClassName("top1")[0].style.backgroundColor = "white" 
+        }
+    }
       function playHihat(e){
-         if(e.code === "KeyE" || event.click === 'hihat'){
+         if(e.code === "KeyE" || e === 'hihat'){
              hihat.currentTime = 0;
              hihat.play();
              const time = Date.now();
-             chanel3.push({
+             chanel1.push({
                  key: e.code,
                  time: time 
              },
              document.getElementsByClassName("top3")[0].style.backgroundColor = "purple"); 
             }
-         console.log(chanel3);
+            else{
+                document.getElementsByClassName("top3")[0].style.backgroundColor = "white" 
+        }
      }
+     function playRide(e){
+        if(e.code === "KeyD" || e === 'ride'){
+            ride.currentTime = 0;
+            ride.play();
+            const time = Date.now();
+            chanel1.push({
+                key: e.code,
+                time: time 
+            },
+            document.getElementsByClassName("mid3")[0].style.backgroundColor = "orange"); 
+           }
+           else{
+               document.getElementsByClassName("mid3")[0].style.backgroundColor = "white" ;
+       }
+   }
+      function playOpenhat(e){
+         if(e.code === "KeyS" || e === 'openhat'){
+             openhat.currentTime = 0;
+             openhat.play();
+             const time = Date.now();
+             chanel1.push({
+                 key: e.code,
+                 time: time 
+             },
+             document.getElementsByClassName("mid2")[0].style.backgroundColor = "yellow"); 
+            }
+            else{
+                document.getElementsByClassName("mid2")[0].style.backgroundColor = "white" ;
+        }
+     }         
+      function playKick(e){
+         if(e.code === "KeyA" || e === 'kick'){
+             kick.currentTime = 0;
+             kick.play();
+             const time = Date.now();
+             chanel1.push({
+                 key: e.code,
+                 time: time 
+             },
+             document.getElementsByClassName("mid1")[0].style.backgroundColor = "lime"); 
+            }
+            else{
+                document.getElementsByClassName("mid1")[0].style.backgroundColor = "white" ;
+        }
+     }  
 
-//RECORD//     
+      function playTom(e){
+         if(e.code === "KeyC" || e === 'tom'){
+             tom.currentTime = 0;
+             tom.play();
+             const time = Date.now();
+             chanel1.push({
+                 key: e.code,
+                 time: time 
+             },
+             document.getElementsByClassName("bottom3")[0].style.backgroundColor = "green"); 
+            }
+            else{
+                document.getElementsByClassName("bottom3")[0].style.backgroundColor = "white" ;
+        }
+     }  
+      function playTink(e){
+         if(e.code === "KeyX" || e === 'tink'){
+             tink.currentTime = 0;
+             tink.play();
+             const time = Date.now();
+             chanel1.push({
+                 key: e.code,
+                 time: time 
+             },
+             document.getElementsByClassName("bottom2")[0].style.backgroundColor = "brown"); 
+            }
+            else{
+                document.getElementsByClassName("bottom2")[0].style.backgroundColor = "white" ;
+        }
+     }  
+      function playSnare(e){
+         if(e.code === "KeyZ" || e === 'snare'){
+             snare.currentTime = 0;
+             snare.play();
+             const time = Date.now();
+             chanel1.push({
+                 key: e.code,
+                 time: time 
+             },
+             document.getElementsByClassName("bottom1")[0].style.backgroundColor = "black"); 
+            }
+            else{
+                document.getElementsByClassName("bottom1")[0].style.backgroundColor = "white" ;
+        }
+     }  
 
-if(document.getElementById('recordButton').clicked == true)
-{
-    document.addEventListener('DOMContentLoaded', () => {
-        'use strict';
-    
-        document.addEventListener('keydown', event => {
-            const charList = 'qweasdzxc';
-            const key = event.key.toLowerCase();
-    
-            // we are only interested in alphanumeric keys
-            if (charList.indexOf(key) === -1) return;
-    
-            console.log(key);
-        });
-    });
-    
-}
+
+
+
+
+
 
 
 
